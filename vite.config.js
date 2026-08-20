16719b32f2ab02fafff30c90a748b9fd434d2fb6
roman1999r <roman.mitkivskiy@gmail.com>
2026-08-20 22:04:32 +0200

    dep

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({plugins:[react()],base:'/cacaoform/'});
+
+export default defineConfig({plugins: [react()], base: '/moldlab/'});
