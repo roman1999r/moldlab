3c72bc56cf2d9d59681bf3aece43592a8ad5c5e1
roman1999r <roman.mitkivskiy@gmail.com>
2026-08-27 17:29:09 +0200

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,28 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import {HashRouter} from 'react-router-dom';
-// import App from './App';
-// import './styles.css';
-// import { AuthProvider } from './context/AuthContext';
-//
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//     <React.StrictMode><HashRouter><AuthProvider>
-//         <App />
-//     </AuthProvider></HashRouter></React.StrictMode>);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-import App from './App';
+import App, { AnalyticsTracker } from './App';
 
 import { AuthProvider } from './context/AuthContext';
 import { LanguageProvider } from './context/LanguageContext';
 
 import './styles.css';
-import AnalyticsTracker from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(
+    document.getElementById('root')
+).render(
     <React.StrictMode>
-            <BrowserRouter basename="/moldlab">
-                {/*<AnalyticsTracker />*/}
-                <LanguageProvider>
-                    <AuthProvider>
-                        <App />
-                    </AuthProvider>
-                </LanguageProvider>
-            </BrowserRouter>
+        <HashRouter>
+
+            <AnalyticsTracker />
+
+            <LanguageProvider>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </LanguageProvider>
+
+        </HashRouter>
     </React.StrictMode>
 );
