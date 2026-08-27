9eaa81a21314d1f59ee2f52682091313eaab74e7
roman1999r <roman.mitkivskiy@gmail.com>
2026-08-27 17:32:25 +0200

    fix main.jsx x2

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 
-import App, { AnalyticsTracker } from './App';
-
 import { AuthProvider } from './context/AuthContext';
 import { LanguageProvider } from './context/LanguageContext';
 
 import './styles.css';
+import App from "./App.jsx";
 
 ReactDOM.createRoot(
     document.getElementById('root')
 ).render(
     <React.StrictMode>
         <HashRouter>
-
-            <AnalyticsTracker />
 
             <LanguageProvider>
                 <AuthProvider>
